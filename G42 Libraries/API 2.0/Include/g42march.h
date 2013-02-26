6652c541c94d54313c4246688639434c65bd8d39
David Ison <developer@dtison.net>
2013-02-26 15:44:59 -0500

    Image Processing Libraries v 2.0, 3.0 ca. 1997

---
+++ G42 Libraries/API 2.0/Include/g42march.h
@@ -0,0 +1,1 @@
+//	Class inspired by PowerPlant UMarchingAnts ©1993-1996 Metrowerks Inc.class	G42MarchingAnts {public:	static Boolean		BeginMarch();	static void			EndMarch();	static void			UseAntsPattern();protected:	static Pattern	sAntsPattern;	static Int32	sNextAntMarchTime;};
---
+++ README.md
@@ -0,0 +1,3 @@
+These sources were maintained under an MSDOS 8.3 naming convention. (So the extensions are capitalised on case-sensitive filesystems)
+
+
